9e4fa2386b8fd5821f036fbe630d4683d7c4e11e
Bert <bertrandmuscat@gmail.com>
2022-12-17 11:25:04 +0100

    étapes 1 et 3

---
+++ scripts/factories/photographer.js
@@ -0,0 +1,34 @@
+function photographerFactory(data) {
+    const { name, city, country, tagline, price, portrait } = data;
+
+    const picture = `assets/photographers/${portrait}`;
+
+    function getUserCardDOM() {
+        
+        const article = document.createElement( 'article' );
+
+        const img = document.createElement( 'img' );
+        img.setAttribute("src", picture);
+
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = name;
+
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+
+        const catchPhrase = document.createElement('p');
+        catchPhrase.textContent = tagline;
+
+        const hourlyFee = document.createElement('p');
+        hourlyFee.textContent = `${price}€/jour`;
+
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(catchPhrase);
+        article.appendChild(hourlyFee);
+        
+        return (article);
+    }
+    return { name, picture, getUserCardDOM };
+}
