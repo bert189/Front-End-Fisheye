bbb8b97556bcb87a0d5c5f5c8ad1c32bd846325e
Bert <bertrandmuscat@gmail.com>
2023-01-14 18:21:36 +0100

    factory media et photographer profile

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,11 +1,66 @@
+// cration de la card img
 
-export function mediaFactory(data) {
+function getImageCardDOM(media, photographer) {
+    
+    const urlImage = `assets/medias/${photographer.name}/${media.image}`;
 
-    const { id } = data;
+    const article = document.createElement( 'article' );
 
-    let photos = [];
+    const img = document.createElement( 'img' );
+    img.setAttribute("src", urlImage);
 
+    const title = document.createElement( 'h3' );
+    h3.textContent = media.title;
 
+    const likes = document.createElement('div');
+    likes.textContent = `${media.likes}&nbsp;&#10084;`;
 
+    article.appendChild(img);
+    article.appendChild(title);
+    article.appendChild(likes);
 
+    return (article);
+      
+
+}
+
+// création de la card video
+
+function getVideoCardDOM(media) {
+    
+    const urlVideo = `assets/images/${media.video}`;
+
+    const article = document.createElement( 'article' );
+    
+    const video = document.createElement( 'video' );
+    img.setAttribute("src", urlVideo);
+
+    const title = document.createElement( 'h3' );
+    h3.textContent = media.title;
+
+    const likes = document.createElement('div');
+    likes.textContent = `${media.likes}&nbsp;&#10084;`;
+
+    article.appendChild(img);
+    article.appendChild(title);
+    article.appendChild(likes);
+
+    return (article);
+    
+
+}
+
+// media factory, usine à cards en fonction du média
+
+export function mediaFactory(media) {
+
+    if (media.image) {
+        return getImageCardDOM(media);
+    }
+    else if (media.video) {
+        return getVideoCardDOM(media);
+    }
+    else {
+        return null;
+    } 
 }
